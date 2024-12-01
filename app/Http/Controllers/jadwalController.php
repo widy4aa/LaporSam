be831f2d9b326c6b202871f1a5d62ac35b1d3249
widy4aa <dio34678@gmail.com>
2024-12-02 05:06:57 +0700

    database seeder with faker

---
+++ app/Http/Controllers/jadwalController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class jadwalController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/tempat_pembuanganController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class tempat_pembuanganController extends Controller
+{
+    //
+}
